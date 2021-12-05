0eff05b1251a44a732eb12f6561d820bda5b8a21
Michael Forero <mforeroroldan@gmail.com>
2021-12-04 22:23:01 -0500

    reorganice el proyecto y su estructura

--- src/databease.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const URI = 'mongodb+srv://andres:12345678Q@cluster0.jmdaw.mongodb.net/MasterDB?retryWrites=true&w=majority';
-
-mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
-module.exports = mongoose;
